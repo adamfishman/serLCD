68d8daddde4541610aa2f7f8d39f07b40435690d
adamfishman <adamfishman@gmail.com>
2014-11-20 13:12:35 -0500

    Update serLCD.h

--- firmware/serLCD.h
+++ firmware/serLCD.h
@@ -22,12 +22,7 @@
 #ifndef serLCD_h
 #define serLCD_h
 
-#if ARDUINO >= 100
 #include "Arduino.h"       // for delayMicroseconds,digitalPinToBitMask, etc
-#else
-#include "WProgram.h"      // for delayMicroseconds
-#include "pins_arduino.h"  // for digitalPinToBitMask, etc
-#endif
 #include "SoftwareSerial.h"
 
 // Commands
